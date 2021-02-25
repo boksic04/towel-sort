4acb346ea2fca49b9b3ef0f1d7bc25eeeb6ae815
Mikalai Butski <77263449+boksic04@users.noreply.github.com>
2021-02-25 22:43:50 +0300

    Add files via upload
    upload new index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = new Array();
+  
+  if(typeof matrix == "undefined" || matrix.length == 0){
+    return arr;
+  }
+  
+  for(let i = 0; i < matrix.length; i++) {
+    if(i%2 == 0) 
+    for(let n = 0; n < matrix[i].length; n++) {
+       arr.push(matrix[i][n]);
+    } else {
+      for(let n = (matrix[i].length-1); n >= 0; n--) {
+        arr.push(matrix[i][n]);
+    }  
+  }
+}
+ return arr;
 }
